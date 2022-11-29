b28fe45b775e458813b0ec0db21fd4ffe385d7c0
LeDuc2004 <114064159+LeDuc2004@users.noreply.github.com>
2022-11-29 10:58:23 +0700

    add file

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      view.setScreenActive("wellcomePage");
+      
+        
+     
+    } else {
+      view.setScreenActive("loginPage");
+    }
+  });
+firebase.firestore().collection("listMessage").doc(`${localStorage.getItem("bothEmail")}`)
+.onSnapshot((doc)=>{
+  view.renderList(doc.data().message)
+  model.getChat()
+})
+firebase.firestore().collection("listMessage").doc(`${localStorage.getItem("bothEmailReverse")}`)
+.onSnapshot((doc)=>{
+  view.renderList(doc.data().message)
+  model.getChat()
+})
+
+firebase.firestore().collection("idName").doc("vGtlozg2ODnWYyeMp04E")
+.onSnapshot((doc)=>{
+  view.rederListFriend(doc.data().idNamechokhac)
+
+})
+
+}
+
+
